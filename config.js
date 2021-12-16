ad1d6c973ae89c9baa597b3118fc05339e370491
hlnhcute <96246242+hlnhcute@users.noreply.github.com>
2021-12-16 21:44:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    titleWeb: "hê lô",
+    introTitle: 'my fen',
+    introDesc: `hlinh cute wa di
+    vdt là ccho của hlinh
+    kkkk
+    hlinh iu ccho`,
+    btnIntro: 'uk',
+    title: 'mẹ hlinh xgai dthw',
+    desc: 'ctrai vdt ngu ngok đúng hok ',
+    btnYes: 'zạ đúng',
+    btnNo: 'éo',
+    question: 'ê nma s m thích t za <3',
+    btnReply: 'gửi cho mẹ iu',
+    reply: 'vì hlinh cute chớ s nữa còn hỏi',
+    mess: 'òm bít mà hi ngại',
+    messDesc: 'xíu bank t 3 tỉ nka đồ thất hứa.',
+    btnAccept: 'òh zạ huhuhuhu',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
